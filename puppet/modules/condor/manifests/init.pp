f2ab8c86ed054e94d91d0f3c72d5a3fd04237771
Neil Williams <neil@reddit.com>
2016-12-10 16:37:57 -0800

    provision

--- puppet/modules/condor/manifests/init.pp
+++ puppet/modules/condor/manifests/init.pp
@@ -1,6 +1,13 @@
 class condor {
+  exec { 'add reddit ppa':
+    command => 'add-apt-repository -y ppa:reddit/ppa',
+    unless  => 'apt-cache policy | grep reddit/ppa',
+    notify  => Exec['update apt cache'],
+  }
+
   $dependencies = [
     'python',
+    'python-baseplate',
     'python-dev',
     'python-gevent',
     'python-setuptools',
@@ -16,5 +23,25 @@ class condor {
     cwd       => $::project_path,
     command   => 'python setup.py develop --user',
     logoutput => true,
+  }
+
+  exec { 'initialize database':
+    user      => $::user,
+    cwd       => $::project_path,
+    command   => 'baseplate-script2 example.ini condor.models:create_schema',
+    logoutput => true,
+    require   => Exec['install app'],
+  }
+
+  file { '/etc/init/condor.conf':
+    ensure => file,
+    source => 'puppet:///modules/condor/condor.upstart',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
+
+  service { 'condor':
+    ensure => running,
   }
 }
